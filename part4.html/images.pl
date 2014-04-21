7fd1a5fcd3faeffd48994abcd8d278eed8e14298
Takuya ASADA <syuu@cloudius-systems.com>
2014-04-22 05:10:37 +0900

    update part4

--- part4.html/images.pl
+++ part4.html/images.pl
@@ -8,6 +8,12 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img1.png"
  ALT="\includegraphics{figures/part4_fig1.eps}">|; 
 
+$key = q/includegraphics{figuresslashpart4_fig2.eps};LFS=12;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="729" HEIGHT="401" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\includegraphics{figures/part4_fig2.eps}">|; 
+
 $key = q/{figure}{centering{preform{<verbatim_mark>verbatim29#preform{{figure};LFS=12;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="188" HEIGHT="108" BORDER="0"
@@ -21,12 +27,6 @@ else
 PPR[7:4] = ISR[7:4]
 PPR[3:0] = 0\end{verbatim}
 \end{figure}">|; 
-
-$key = q/includegraphics{figuresslashpart4_fig2.eps};LFS=12;FSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="729" HEIGHT="401" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="\includegraphics{figures/part4_fig2.eps}">|; 
 
 1;
 
--- part4.html/labels.pl
+++ part4.html/labels.pl
@@ -2,11 +2,11 @@
 # Associate labels original text with physical files.
 
 
-$key = q/table2/;
-$external_labels{$key} = "$URL/" . q|node7.html|; 
+$key = q/table1/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_interrupt-routing/;
+$key = q/cite_x2APIC/;
 $external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
@@ -14,15 +14,15 @@ $key = q/fig1/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/table2/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/fig2/;
 $external_labels{$key} = "$URL/" . q|node5.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/table1/;
-$external_labels{$key} = "$URL/" . q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_x2APIC/;
+$key = q/cite_interrupt-routing/;
 $external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
@@ -33,20 +33,20 @@ $noresave{$key} = "$nosave";
 # labels from external_latex_labels array.
 
 
-$key = q/table2/;
-$external_latex_labels{$key} = q|2|; 
+$key = q/table1/;
+$external_latex_labels{$key} = q|1|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig1/;
 $external_latex_labels{$key} = q|1|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig2/;
+$key = q/table2/;
 $external_latex_labels{$key} = q|2|; 
 $noresave{$key} = "$nosave";
 
-$key = q/table1/;
-$external_latex_labels{$key} = q|1|; 
+$key = q/fig2/;
+$external_latex_labels{$key} = q|2|; 
 $noresave{$key} = "$nosave";
 
 1;
