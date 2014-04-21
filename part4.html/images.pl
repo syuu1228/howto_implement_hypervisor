0b118c39f9188409eace7a327b9cc004e0da97f1
Takuya ASADA <syuu@cloudius-systems.com>
2014-04-22 04:52:18 +0900

    Add part4

---
+++ part4.html/images.pl
@@ -0,0 +1,32 @@
+# LaTeX2HTML 2012 (1.2)
+# Associate images original text with physical files.
+
+
+$key = q/includegraphics{figuresslashpart4_fig1.eps};LFS=12;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="803" HEIGHT="494" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\includegraphics{figures/part4_fig1.eps}">|; 
+
+$key = q/{figure}{centering{preform{<verbatim_mark>verbatim29#preform{{figure};LFS=12;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="188" HEIGHT="108" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\begin{figure}\centering
+\begin{verbatim}/* [7:4] は優先度クラス */
+/* ...
+...4]
+PPR = TPR
+else
+PPR[7:4] = ISR[7:4]
+PPR[3:0] = 0\end{verbatim}
+\end{figure}">|; 
+
+$key = q/includegraphics{figuresslashpart4_fig2.eps};LFS=12;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="729" HEIGHT="401" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\includegraphics{figures/part4_fig2.eps}">|; 
+
+1;
+
---
+++ part4.html/labels.pl
@@ -0,0 +1,53 @@
+# LaTeX2HTML 2012 (1.2)
+# Associate labels original text with physical files.
+
+
+$key = q/table2/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_interrupt-routing/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig1/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig2/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table1/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_x2APIC/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2012 (1.2)
+# labels from external_latex_labels array.
+
+
+$key = q/table2/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig1/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig2/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table1/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+1;
+
